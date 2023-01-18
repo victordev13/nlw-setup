dd93edf8c9206cd12b9d06e36f9f81f30dc931e5
victordev13 <vtrcarvalho.13@gmail.com>
2023-01-18 12:35:28 -0300

    feat: server routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,11 @@
-import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'T'
-      }
-    }
-  });
-
-  return {habits}
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333
