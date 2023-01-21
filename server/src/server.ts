d2f6a5152379ef786e52968c575081f945c440b4
victordev13 <vtrcarvalho.13@gmail.com>
2023-01-21 20:59:07 -0300

    fix: (server) fastify host config to use ip address

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import Fastify from 'fastify';
-import cors from '@fastify/cors'
+import cors from '@fastify/cors';
 import { appRoutes } from './routes';
 
-const app = Fastify()
+const app = Fastify();
 
-app.register(cors)
-app.register(appRoutes)
+app.register(cors);
+app.register(appRoutes);
 
-app.listen({
-  port: 3333
-}).then(() => console.log('Server running at port 3333...'))
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => console.log('Server running at port 3333...'));
